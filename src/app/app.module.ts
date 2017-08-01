5b37ce5e04239a55e411f1506be24fb442ea0957
Damon Collins -X (damcolli - TEKSYSTEMS INC at Cisco) <damcolli@cisco.com>
2017-08-01 14:09:32 -0700

    main app components and switch proj to SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { ViewListComponent } from './shopping-list/view-list/view-list.component';
+import { EditListComponent } from './shopping-list/edit-list/edit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ViewListComponent,
+    EditListComponent
   ],
   imports: [
     BrowserModule
